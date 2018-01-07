6b71578df1b438ce4531322ac32e1e6684f75c61
Sergey Gladyshev <gsu1234@mail.ru>
2018-01-07 17:58:48 +0900

    #2

---
+++ anastosi22.ru [ForkCMS]/src/Frontend/Modules/Slideshow/Actions/Index.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Frontend\Modules\Slideshow\Actions;
+
+use Frontend\Core\Engine\Base\Block as FrontendBaseBlock;
+use Frontend\Core\Engine\Navigation as FrontendNavigation;
+use Frontend\Modules\Slideshow\Engine\Model as FrontendSlideshowModel;
+
+/**
+ * This is the index-action
+ *
+ * @author Koen Vinken <koen@tagz.be>
+ */
+class Index extends FrontendBaseBlock
+{
+    /**
+     * Execute the extra
+     *
+     * @return void
+     */
+    public function execute()
+    {
+        // call the parent
+        parent::execute();
+
+        // load template
+        $this->loadTemplate();
+
+        // load the data
+        $this->getData();
+
+        // parse
+        $this->parse();
+    }
+
+    /**
+     * Get the data
+     *
+     * @return void
+     */
+    private function getData()
+    {
+        //get all galleries
+        $this->item = FrontendSlideshowModel::getGalleries();
+
+        //full url asset
+        $this->full_url = FrontendNavigation::getURLForBlock('Slideshow', 'detail');
+        $this->full_url_category = FrontendNavigation::getURLForBlock('Slideshow', 'category');
+    }
+
+    /**
+     * Parse
+     *
+     * @return void
+     */
+    private function parse()
+    {
+        //assign
+        $this->tpl->assign('full_url', $this->full_url);
+        $this->tpl->assign('full_url_category', $this->full_url_category);
+        $this->tpl->assign('galleries', $this->item);
+    }
+}
