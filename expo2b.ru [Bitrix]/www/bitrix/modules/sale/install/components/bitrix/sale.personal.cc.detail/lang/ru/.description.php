3ca11bc79380df0149ab2146dc3a35456f4c2660
Resident234 <FqjJrDCxSG8>
2018-01-27 23:37:25 +0900

    2336

--- expo2b.ru [Bitrix]/www/bitrix/modules/sale/install/components/bitrix/sale.personal.cc.detail/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS ['SPCD_DEFAULT_TEMPLATE_NAME'] = "–едактирование пластиковых карт";
-$MESS ['SPCD_DEFAULT_TEMPLATE_DESCRIPTION'] = "ѕозвол€ет редактировать пластиковые карты пользовател€";
-$MESS ['SPCD_NAME'] = "ѕерсональный раздел";
-?>
