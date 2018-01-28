da77d5a196c4ca32b11fbc617d02b92f943e3b4d
Resident234 <FqjJrDCxSG8>
2018-01-28 12:58:07 +0900

    1252

--- expo2b.ru [Bitrix]/www/bitrix/modules/sale/install/version.php
+++
@@ -1,5 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "16.0.21",
-	"VERSION_DATE" => "2016-04-11 16:00:00"
-);
--- expo2b.ru [Bitrix]/www/bitrix/modules/sale/lang/en/admin/pay_system_ajax.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS["SALE_PS_SORT"] = "Sort";
-$MESS["SALE_PS_TARIFF"] = "Tariffs";
-$MESS["SALE_PS_PRICE_INFO"] = "Using order currency";
-?>
